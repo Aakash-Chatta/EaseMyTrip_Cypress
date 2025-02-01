a7d09aa8a85e77a25fe93972203129377a851f83
Aakash Chatta <aakashchatta@gmail.com>
2025-02-01 22:59:40 +0530

    fixed flight test

--- cypress/e2e/tests/Hotel_test.js
+++ cypress/e2e/tests/Hotel_test.js
@@ -1,6 +1,12 @@
 import { Given, Then} from "cypress-cucumber-preprocessor/steps";
 import pg1 from "../pages/Hotel_page";
 
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
+
 Given("User navigates to EaseMyTrip Hotels website", () => {
     pg1.enterURL()
 });
