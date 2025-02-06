638ec797f041e26d6434fe8fd7da1fa80b1fb350
Aakash Chatta <aakashchatta@gmail.com>
2025-02-06 14:35:34 +0530

    added train tests

--- cypress/e2e/tests/train_test.js
+++ cypress/e2e/tests/train_test.js
@@ -19,6 +19,21 @@ Then("User selects to city station name as {string}",  (city)=> {
 And("User selects departure date as {string} month as {string} year as {string}",  (date,month,year)=> {
     train.enter_travel_date(date,month,year)
 })
-When("User selects train no as {string} and selects class as {string} if available,other wise selects whatever is available", (train_number)=> {
-    train.select_train_and_class(train_number)
+When("User selects train no as {string} and selects class as {string}", (train_number,class_type)=> {
+    train.select_train_and_class(train_number,class_type)
+})
+Then("User Selects UserID as {string}",(userID)=>{
+    train.enter_userID(userID)
+})
+Then("User selects free cancellation",()=>{
+    train.opt_in_for_cancellation()
+})
+Then("User enters email as {string} and phone number as {string} in Contact Information",(email,phone_number)=>{
+    train.contact_information(email,phone_number)
+})
+Then("User enters Name as {string} age as {string} Berth as {string} meal as {string} and gender as {string} in Traveller Information",(name,age,berth,meal,gender)=>{
+    train.traveller_information(name,age,berth,meal,gender)
+})
+Then("User Selects insurance and enables auto upgradation",()=>{
+    train.insurance()
 })
